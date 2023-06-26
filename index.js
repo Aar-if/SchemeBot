93f0eb269da6834f6fc1ff3d925d103ae25a0010
Arif <arifmajeed007.as@gmail.com>
2023-06-26 18:03:21 +0530

    Latest commit

--- index.js
+++ index.js
@@ -8,86 +8,121 @@ app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 app.get("/multilang", async (req, res) => {
-  const requestData = req;
-  // console.log(requestData);
-  console.log("ENTERED INTO FIRST API CALL FOR LANGUAGE CONERSION");
-  await axios
-    .post("	https://demo-api.models.ai4bharat.org/inference/translation/v2", {
-      controlConfig: { dataTracking: true },
-      input: [
-        {
-          source:
-            "मैं किरण हूं. मैं 10वीं कक्षा में पढ़ने वाले एक बच्चे का माता-पिता हूं। मेरी सालाना आय 2.4 लाख रुपये सालाना है. मेरे लिए कौन सी योजनाएँ उपलब्ध हैं?",
+  const requestData = req.query.query_string;
+  const inputLang = req.query.inputLanguage;
+  const outputLang = req.query.outputLanguage;
+  console.log(requestData);
+  console.log(inputLang);
+  console.log(outputLang);
+
+  console.log("ENTERED INTO FIRST API CALL FOR LANGUAGE CONVERSION");
+
+  // Check if input language is 'en'
+  if (inputLang !== "en") {
+    await axios
+      .post("https://demo-api.models.ai4bharat.org/inference/translation/v2", {
+        controlConfig: { dataTracking: true },
+        input: [
+          {
+            source: requestData,
+          },
+        ],
+        config: {
+          serviceId: "",
+          language: {
+            sourceLanguage: inputLang,
+            targetLanguage: "en",
+            targetScriptCode: null,
+            sourceScriptCode: null,
+          },
         },
-      ],
-      config: {
-        serviceId: "",
-        language: {
-          sourceLanguage: "hi",
-          targetLanguage: "en",
-          targetScriptCode: null,
-          sourceScriptCode: null,
+      })
+      .then(async (response) => {
+        const responseData = response.data;
+        console.log(responseData);
+        console.log("CONVERTED FROM INDIC TO ENGLISH");
+        console.log(responseData.output[0].target);
+        makeSecondApiCall(responseData.output[0].target);
+      })
+      .catch((error) => {
+        console.error("Error fetching data from the API 1 :", error);
+        res.status(500).json({ error: "An error occurred" });
+      });
+  } else {
+    // Skip first API call, directly make the second API call
+    makeSecondApiCall(requestData);
+  }
+
+  async function makeSecondApiCall(text) {
+    console.log(
+      "ENTERED INTO SECOND API CALL WHICH WILL SEND CONVERTED ENGLISH DATA TO JUGALBANDI"
+    );
+    await axios
+      .get("http://4.240.112.55:8000/query-with-gptindex", {
+        params: {
+          uuid_number: "effc6ca6-1181-11ee-9884-0242ac110002",
+          query_string: text,
         },
-      },
-    })
-    .then(async (response) => {
-      const responseData = response.data;
-      res.json(responseData);
-      console.log(responseData);
-      console.log("CONVERTED FROM INDIC TO ENGLISH");
-      console.log(responseData.output[0].target);
+        headers: {
+          accept: "application/json",
+        },
+      })
+      .then(async (response) => {
+        const responseData = response.data.answer;
+        console.log("GOT THE SECOND API RESPONSE FROM JUGALBANDI IN ENGLISH");
+        console.log(responseData);
+        makeThirdApiCall(responseData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data from the API 2:", error);
+        res.status(500).json({ error: "An error occurred" });
+      });
+  }
+
+  async function makeThirdApiCall(text) {
+    // Check if output language is 'en'
+    if (outputLang !== "en") {
       console.log(
-        "ENTERED INTO SECOND API CALL WHICH WILL SEND CONVERTED ENGLISH DATA TO JUGALBANDI"
+        "ENTERED INTO THIRD API CALL TO CONVERT THE ENGLISH RESPONSE BACK TO INDIC LANGUAGE"
       );
       await axios
-        .get("http://4.240.112.55:8000/query-with-gptindex", {
-          params: {
-            uuid_number: "effc6ca6-1181-11ee-9884-0242ac110002",
-            query_string: responseData.output[0].target,
-          },
-          headers: {
-            accept: "application/json",
-          },
-        })
-        .then(async (response) => {
-          const responseData = response.data.answer;
-          console.log("GOT THE SECOND API RESPONSE FROM JUGALBANDI IN ENGLISH");
-          console.log(responseData);
-          console.log(
-            "ENTERED INTO THIRD API CALL TO CONVERT THE ENGLISH RESPONSE BACK TO INDIC LANGUAGE"
-          );
-          await axios
-            .post(
-              "https://demo-api.models.ai4bharat.org/inference/translation/v2",
+        .post(
+          "https://demo-api.models.ai4bharat.org/inference/translation/v2",
+          {
+            controlConfig: { dataTracking: true },
+            input: [
               {
-                controlConfig: { dataTracking: true },
-                input: [
-                  {
-                    source: responseData,
-                  },
-                ],
-                config: {
-                  serviceId: "",
-                  language: {
-                    sourceLanguage: "en",
-                    targetLanguage: "hi",
-                    targetScriptCode: null,
-                    sourceScriptCode: null,
-                  },
-                },
-              }
-            )
-            .then(async (response) => {
-              const responseData = response.data;
-              console.log("GOT THE THIRD API RESPONSE IN INDIC LANGUAGE");
-              console.log(responseData.output[0].target);
-            });
+                source: text,
+              },
+            ],
+            config: {
+              serviceId: "",
+              language: {
+                sourceLanguage: "en",
+                targetLanguage: outputLang,
+                targetScriptCode: null,
+                sourceScriptCode: null,
+              },
+            },
+          }
+        )
+        .then(async (response) => {
+          const responseData = response.data;
+          console.log("GOT THE THIRD API RESPONSE IN INDIC LANGUAGE");
+          console.log(responseData.output[0].target);
+
+          const botResponse = responseData.output[0].target;
+          res.json({ answer: botResponse });
+        })
+        .catch((error) => {
+          console.error("Error fetching data from the API 3:", error);
+          res.status(500).json({ error: "An error occurred" });
         });
-    })
-    .catch((error) => {
-      console.error("Error fetching data from the other API:", error);
-      res.status(500).json({ error: "An error occurred" });
-    });
+    } else {
+      // Skip third API call, directly return the response
+      res.json({ answer: text });
+    }
+  }
 });
 
 app.listen(port, () => {
